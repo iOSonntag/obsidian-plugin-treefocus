70c4688bbd18b9f6296739a8925eb93d39aee351
iOSonntag <iosonntag@iOSonntag.fritz.box>
2023-09-24 06:59:18 +0200

    feat(ci): improve ci
    - added semantic-version package to automate CHANGELOG.md creation and version bumping
    - added husky commit message hook to prevent commit messages not parsable by semantic-version package

---
+++ .releaserc.js
@@ -0,0 +1,64 @@
+module.exports = {
+  "branches": [
+    "+([0-9])?(.{+([0-9]),x}).x",
+    "main",
+    "master",
+    "next",
+    "next-major",
+    {
+      "name": "beta",
+      "prerelease": true
+    },
+    {
+      "name": "alpha",
+      "prerelease": true
+    }
+  ],
+  "plugins": [
+    [
+      "@semantic-release/commit-analyzer",
+      {
+        "preset": "angular",
+        "releaseRules": [
+          {
+            "type": "breaking",
+            "release": "major"
+          }
+        ]
+      }
+    ],
+    // [
+    //   "@semantic-release/exec",
+    //   {
+    //     "verifyReleaseCmd": "echo \"NEXT_RELEASE_VERSION=${nextRelease.version}\" >> $GITHUB_OUTPUT"
+    //   }
+    // ],
+    "@semantic-release/release-notes-generator",
+    [
+      "@semantic-release/changelog",
+      {
+        "changelogFile": "CHANGELOG.md"
+      }
+    ],
+    [
+      "@semantic-release/npm",
+      {
+        "npmPublish": false 
+      }
+    ],
+    // "@semantic-release/github",
+    // [
+    //   "@semantic-release/git",
+    //   {
+    //     "assets": [
+    //       "package.json",
+    //       "package-lock.json",
+    //       "CHANGELOG.md"
+    //     ],
+    //     "message": "chore(release): v${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+    //   }
+    // ]
+  ],
+  "dryRun": true,
+  "preset": "angular"
+};
